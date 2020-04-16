4695c9b079b2f8339038694109aeb29475be958f
LampV <1071952403@qq.com>
2020-04-16 23:24:03 +0800

    新建__new__方法，保证title对应的实例唯一

--- expdf/graph.py
+++ expdf/graph.py
@@ -3,16 +3,38 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-16 22:43
+@edit time: 2020-04-16 23:23
 @FilePath: /expdf/graph.py
 @desc: 制作PDF图结构
 """
 from collections import namedtuple
 from .parser import ExPDFParser
 
+
 class PDFNode:
+    instances = {}
+
+    def __new__(cls, title, refs=None):
+        """保证每个title对应一个PDFNode对象
+        创建对象的时候如果title已经有对应对象，就返回该对象
+        若没有，则新建对象
+        
+        对于有对应对象的情况，还要检查refs是否相等。因为这相当于一次
+        get，不能改变属性
+        """
+        if title in cls.instances:
+            obj = cls.instances[title]
+            if obj.actients == refs or refs is None:
+                return obj
+            else:
+                raise TypeError("Can't instantiate PDFNode with same title but different refs")
+        else:
+            obj = object.__new__(cls)
+            cls.instances[title] = obj
+            return obj
+
     def __init__(self, title, refs=[]):
         self.title = title
         self.parents = []
-        self.actients = [PDFNode(ref) for ref in refs]
-
+        if not hasattr(self, 'actients'):
+            self.actients = [PDFNode(ref) for ref in refs]
