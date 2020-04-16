4806f5a124e3a2e72037cce70d5da22761b403e7
LampV <1071952403@qq.com>
2020-04-16 22:36:37 +0800

    rename class and funciton

--- expdf/__init__.py
+++ expdf/__init__.py
@@ -3,9 +3,9 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-16 22:12
+@edit time: 2020-04-16 22:27
 @FilePath: /expdf/__init__.py
 @desc: init file of expdf
 """
 
-from .parser import expdf_parser, ExPDF
+from .parser import ex_parser, ExPDFParser
