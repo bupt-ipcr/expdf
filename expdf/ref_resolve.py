0d57aa709ceee8c3807f48b1fe2db92db87de057
LampV <1071952403@qq.com>
2020-04-16 16:35:03 +0800

    更新函数

--- expdf/ref_resolve.py
+++ expdf/ref_resolve.py
@@ -3,7 +3,7 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-16 16:29
+@edit time: 2020-04-16 16:34
 @FilePath: /expdf/ref_resolve.py
 @desc: 解析PDF中的
 
@@ -43,38 +43,38 @@ def get_refs(uri):
     return refs
 
 
-def process_PDFObjRef(pdfobj):
+def resolve_PDFObjRef(pdfobj):
     """处理PDF对象，递归查找所有引用并返回
     @param pdfobj: PDFObjRef 对象
     @return: list or nesting lists or None
     """
     if isinstance(pdfobj, list):
-        return [process_PDFObjRef(item) for item in pdfobj]
+        return [resolve_PDFObjRef(item) for item in pdfobj]
 
-    # print(">", pdfobj, type(pdfobj))
     if not isinstance(pdfobj, PDFObjRef):
-        # print("type not of PDFObjRef")
         return None
 
+    # 如果是PDFObjRef，则将其resolve之后继续判断
     obj_resolved = pdfobj.resolve()
-    # print("obj_resolved:", obj_resolved, type(obj_resolved))
+    
+    # 优先进行短路判断
+    if isinstance(obj_resolved, str):
+        return get_refs(obj_resolved)
+    
+    # bytes: decode
     if isinstance(obj_resolved, bytes):
         obj_resolved = obj_resolved.decode("utf-8")
 
-    if isinstance(obj_resolved, str):
-        return get_refs(obj_resolved)
-
+    # list: recursive resolve
     if isinstance(obj_resolved, list):
-        return [process_PDFObjRef(o) for o in obj_resolved]
+        return [resolve_PDFObjRef(o) for o in obj_resolved]
 
+    # process resource
     if "URI" in obj_resolved:
         if isinstance(obj_resolved["URI"], PDFObjRef):
-            return process_PDFObjRef(obj_resolved["URI"])
+            return resolve_PDFObjRef(obj_resolved["URI"])
 
     if "A" in obj_resolved:
         if isinstance(obj_resolved["A"], PDFObjRef):
-            return process_PDFObjRef(obj_resolved["A"])
+            return resolve_PDFObjRef(obj_resolved["A"])
 
-        if "URI" in obj_resolved["A"]:
-            # print("->", a["A"]["URI"])
-            return get_refs(obj_resolved["A"]["URI"].decode("utf-8"))
