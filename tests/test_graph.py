7cafeb721747b800d405d994f229a51e9eefa18a
LampV <1071952403@qq.com>
2020-04-16 23:24:10 +0800

    完成对应test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,7 +3,7 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-16 22:46
+@edit time: 2020-04-16 23:20
 @FilePath: /tests/test_graph.py
 @desc: 
 """
@@ -12,10 +12,23 @@ from expdf.graph import PDFNode
 
 def test_newnode():
     """测试node的new方法"""
-    n0 = PDFNode('title0')
+    n0 = PDFNode('title0', refs=['ta', 'tb'])
+    
+    # 创建和n0同名对象时，覆盖refs会报错
+    has_except = False
+    try:
+        n0copy = PDFNode('title0', [])
+    except Exception as e:
+        assert str(e) == "Can't instantiate PDFNode with same title but different refs"
+        has_except = True
+    assert has_except == True, "Create two instance of PDFNode with different refs has no error"
+    
+    # 不指明refs则不会报错
     n0copy = PDFNode('title0')
-    n1 = PDFNode('title1')
     
     # n0 和 n0copy 应该是一个对象
     assert n0 is n0copy
-    assert n0 is not n1
+    # n0的属性应该正确
+    assert n0.title == 'title0'
+    # n0的refs不应该被覆盖
+    assert n0.actients == [PDFNode('ta'), PDFNode('tb')]
