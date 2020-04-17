42bd00f6d13bdc5ec3e117904a9a6a71f0e60fa4
LampV <1071952403@qq.com>
2020-04-17 10:48:05 +0800

    modify utils test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,24 +3,24 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-16 22:35
+@edit time: 2020-04-17 10:47
 @FilePath: /tests/test_utils.py
 @desc: 测试utils
 """
 
-from expdf.utils import Reference, flatten
+from expdf.utils import Link, flatten
 
 def test_flatten():
-    s1 = Reference(1, 'a')
-    s2 = Reference(2, 'a')
-    s3 = Reference(3, 'a')
-    s4 = Reference(4, 'a')
-    s5 = Reference(5, 'a')
+    s1 = Link(1, 'a', 1)
+    s2 = Link(2, 'a', 2)
+    s3 = Link(3, 'a', 3)
+    s4 = Link(4, 'a', 4)
+    s5 = Link(5, 'a', 5)
     
     l = [s1, [s2, [s3, [s4, None]]]]
     fl = flatten(l)
     assert fl == [s1, s2, s3, s4]
     
-    l = [s2, [s2, s3, s4], s5]
+    l = [s1, [s2, s3, s4], s5]
     fl = flatten(l)
     assert fl == [s1, s2, s3, s4, s5]
