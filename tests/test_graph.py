64a9928e785014ab5b972f159016ad7dc782cf01
LampV <1071952403@qq.com>
2020-04-17 11:34:06 +0800

    重构测试方法

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,45 +3,68 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-17 11:19
+@edit time: 2020-04-17 11:33
 @FilePath: /tests/test_graph.py
 @desc: 
 """
 
 from expdf.graph import PDFNode
 
-def test_newnode():
-    """测试node的new方法"""
-    n0 = PDFNode('title0', refs=['ta', 'tb'])
-    
-    
-    # 创建和n0同名对象时，使用相同ref应该不报错
-    has_except = False
-    try:
+
+class TestPDFNode:
+    @classmethod
+    def setup_class(cls):
+        PDFNode.clear_nodes()
+        
+    @classmethod
+    def teardown_class(cls):
+        PDFNode.clear_nodes()
+
+    def test_new(self):
+        """测试新建node是否表现正常"""
         n0 = PDFNode('title0', refs=['ta', 'tb'])
-    except Exception as e:
-        has_except = True
-    assert has_except == False, "Unexpected error in creating PDFNode instance"
-    
-    has_except = False
-    try:
-        n0copy = PDFNode('title0', [])
-    except Exception as e:
-        assert str(e) == "Can't instantiate PDFNode with same title but different refs"
-        has_except = True
-    assert has_except == True, "Create two instance of PDFNode with different refs has no error"
-    
-    # 不指明refs则不会报错
-    n0copy = PDFNode('title0')
-    
-    # n0 和 n0copy 应该是一个对象
-    assert n0 is n0copy
-    # n0的属性应该正确
-    assert n0.title == 'title0'
-    # n0的refs不应该被覆盖
-    assert n0.actients == [PDFNode('ta'), PDFNode('tb')]
-    
-    # ta对象应该有posterity，但是没有children
-    na = PDFNode('ta')
-    assert na.children == set()
-    assert na.posterity == {n0}
+        
+        # 创建和n0同名对象时，使用相同ref应该不报错
+        has_except = False
+        try:
+            n0 = PDFNode('title0', refs=['ta', 'tb'])
+        except Exception as e:
+            has_except = True
+        assert has_except == False, "Unexpected error in creating PDFNode instance"
+        
+        # 使用不同ref应该会报错
+        has_except = False
+        try:
+            n0copy = PDFNode('title0', [])
+        except Exception as e:
+            assert str(e) == "Can't instantiate PDFNode with same title but different refs"
+            has_except = True
+        assert has_except == True, "Create two instance of PDFNode with different refs has no error"
+        
+        # 不指明refs则不会报错
+        has_except = False
+        try:
+            n0copy = PDFNode('title0')
+        except Exception as e:
+            has_except = True
+        assert has_except == False, "Unexpected error in creating PDFNode instance"
+        
+        # n0 和 n0copy 应该是一个对象
+        assert n0 is n0copy
+        
+        # 所有用到的node都应该被创建
+        nodes = PDFNode.get_nodes() 
+        assert nodes == [n0, PDFNode('ta'), PDFNode('tb')]
+
+
+    def test_relations(self):
+        """测试节点之间祖先/子孙关系是否正常"""
+        n0 = PDFNode('title0', refs=['ta', 'tb'])
+        na, nb = PDFNode('ta'), PDFNode('tb')
+        
+        # n0 祖先应该是ta, tb
+        assert n0.actients == {na, nb}
+        
+        # ta对象应该有posterity，但是没有children
+        assert na.children == set()
+        assert na.posterity == {n0}
