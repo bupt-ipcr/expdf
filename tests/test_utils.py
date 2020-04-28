ab972a3d1175f4be11109cc5317a44b8342fba46
LampV <1071952403@qq.com>
2020-04-28 12:09:30 +0800

    format file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,12 +3,13 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-17 10:47
+@edit time: 2020-04-28 12:09
 @FilePath: /tests/test_utils.py
 @desc: 测试utils
 """
 
 from expdf.utils import Link, flatten
+
 
 def test_flatten():
     s1 = Link(1, 'a', 1)
@@ -16,11 +17,11 @@ def test_flatten():
     s3 = Link(3, 'a', 3)
     s4 = Link(4, 'a', 4)
     s5 = Link(5, 'a', 5)
-    
-    l = [s1, [s2, [s3, [s4, None]]]]
-    fl = flatten(l)
-    assert fl == [s1, s2, s3, s4]
-    
-    l = [s1, [s2, s3, s4], s5]
-    fl = flatten(l)
-    assert fl == [s1, s2, s3, s4, s5]
+
+    l1 = [s1, [s2, [s3, [s4, None]]]]
+    fl1 = flatten(l1)
+    assert fl1 == [s1, s2, s3, s4]
+
+    l2 = [s1, [s2, s3, s4], s5]
+    fl2 = flatten(l2)
+    assert fl2 == [s1, s2, s3, s4, s5]
