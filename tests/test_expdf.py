dd3e7e47290458bb5cc45515ae8b3629fc72a752
LampV <1071952403@qq.com>
2020-04-30 12:13:36 +0800

    update test case

--- tests/test_expdf.py
+++ tests/test_expdf.py
@@ -3,7 +3,7 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-30 11:17
+@edit time: 2020-04-30 12:12
 @FilePath: /tests/test_expdf.py
 @desc: 测试expdf
 """
@@ -32,17 +32,17 @@ class TestExPDF:
 
     def test_refs(self):
         aims = [
-            'Wirelesscaching: technical misconceptions and business barriers,',
-            'Optimal Cooperative Content Cachingand Delivery Policy for Heterogeneous Cellular Networks,', 
-            'Joint Caching, Routing,and Channel Assignment for Collaborative Small-Cell Cellular Net-works,',
-            'On the Complexity of Optimal ContentPlacement in Hierarchical Caching Networks,',
-            'L. Lei, D. Yuan, C. K. Ho, and S. Sun, “A uniﬁed graph labelingalgorithm for consecutive-block channel allocation in SC-FDMA, IEEETrans. Wireless Commun., vol. 12, no. 11, pp. 5767-5779, Nov. 2013',
-            'Efﬁcient processing of deepneural networks: A tutorial and survey',
+            'Wireless caching: technical misconceptions and business barriers,',
+            'Optimal Cooperative Content Caching and Delivery Policy for Heterogeneous Cellular Networks,', 
+            'Joint Caching, Routing, and Channel Assignment for Collaborative Small-Cell Cellular Net-works,',
+            'On the Complexity of Optimal Content Placement in Hierarchical Caching Networks,',
+            'L. Lei, D. Yuan, C. K. Ho, and S. Sun, “A uniﬁed graph labeling algorithm for consecutive-block channel allocation in SC-FDMA, IEEE Trans. Wireless Commun., vol. 12, no. 11, pp. 5767-5779, Nov. 2013',
+            'Efﬁcient processing of deep neural networks: A tutorial and survey',
             'Resource Man-agement with Deep Reinforcement Learning',
-            'Edge Caching atBase Stations with Device-to-Device Ofﬂoading',
-            'Optimal cell clustering andactivation for energy saving in load-coupled wireless networks,',
+            'Edge Caching at Base Stations with Device-to-Device Ofﬂoading',
+            'Optimal cell clustering and activation for energy saving in load-coupled wireless networks,',
             'K. Murty, Linear programming, Wiley, 1983.',
-            'Minimum-Time LinkScheduling for Emptying Wireless Systems: Solution Characterizationand Algorithmic Framework,'
+            'Minimum-Time Link Scheduling for Emptying Wireless Systems: Solution Characterization and Algorithmic Framework,'
         ]
         
         assert self.expdf_parser.refs == aims
