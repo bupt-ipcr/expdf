bddf7b2d82661408b837952c980a4884772892b3
LampV <1071952403@qq.com>
2020-04-30 12:13:51 +0800

    add test case

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -3,7 +3,7 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-30 11:23
+@edit time: 2020-04-30 12:12
 @FilePath: /tests/test_extractor.py
 @desc: 测试extractor中匹配效果
 """
@@ -21,13 +21,19 @@ def test_get_ref_title():
     # 引号引用类型
     ref_1 = '''W. Jiang, G. Feng and S. Qin, “Optimal Cooperative Content Cachingand Delivery Policy for Heterogeneous Cellular Networks,” in IEEETransactions on Mobile Computing, vol. 16, no. 5, pp. 1382-1393, May2017.'''
     assert get_ref_title(ref_1) == 'Optimal Cooperative Content Cachingand Delivery Policy for Heterogeneous Cellular Networks,'
+    
     # 分句引用类型
     ref_2 = '''L. Breslau, Pei Cao, Li Fan, G. Phillips, and S. Shenker. Web caching and zipf-like distributions: evidence and implications. In INFOCOM ’99. Eighteenth Annual Joint Conference of the IEEE Computer and Communications Societies. Proceedings. IEEE, volume 1, pages 126–134 vol.1, Mar 1999.'''
     assert get_ref_title(ref_2) == 'Web caching and zipf-like distributions: evidence and implications'
+    
+    ref_2_a = '''Abounadi, J., Bertsekas, D. and Borkar, V. S. (2001). Learning algorithms for Markov Decision  Processes with average cost. SIAM Journal on Control and Optimization, 40 681–698.'''
+    assert get_ref_title(ref_2_a) == 'Learning algorithms for Markov Decision Processes with average cost'
+    
     # 暂时无法匹配的类型
     ref_3 = '''My title.: “123”, 1998'''
     assert get_ref_title(ref_3) == 'My title.: “123”, 1998'
     # strict mode
+    
     assert get_ref_title(ref_1, strict=True) == 'Optimal Cooperative Content Cachingand Delivery Policy for Heterogeneous Cellular Networks,'
     assert get_ref_title(ref_2, strict=True) == 'Web caching and zipf-like distributions: evidence and implications'
     assert get_ref_title(ref_3, strict=True) is None
