f3544a2bdb1c77d32642c4f4d810fcaca5a43180
LampV <1071952403@qq.com>
2020-04-30 11:20:20 +0800

    update test case

--- tests/test_expdf.py
+++ tests/test_expdf.py
@@ -3,7 +3,7 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-04-29 20:45
+@edit time: 2020-04-30 11:17
 @FilePath: /tests/test_expdf.py
 @desc: 测试expdf
 """
@@ -37,8 +37,8 @@ class TestExPDF:
             'Joint Caching, Routing,and Channel Assignment for Collaborative Small-Cell Cellular Net-works,',
             'On the Complexity of Optimal ContentPlacement in Hierarchical Caching Networks,',
             'L. Lei, D. Yuan, C. K. Ho, and S. Sun, “A uniﬁed graph labelingalgorithm for consecutive-block channel allocation in SC-FDMA, IEEETrans. Wireless Commun., vol. 12, no. 11, pp. 5767-5779, Nov. 2013',
-            'V. Sze, Y. Chen, T. Yang, and J. Emer, “Efﬁcient processing of deepneural networks: A tutorial and survey”, http://arxiv.org/abs/1703.09039,Mar. 2017.',
-            'H. Mao, M. Alizadeh, I. Menache, and S. Kandula. “Resource Man-agement with Deep Reinforcement Learning”, In Proceedings of the 15thACM Workshop on Hot Topics in Networks (HotNets). ACM, New York,USA, 50-56, 2016.',
+            'Efﬁcient processing of deepneural networks: A tutorial and survey',
+            'Resource Man-agement with Deep Reinforcement Learning',
             'Edge Caching atBase Stations with Device-to-Device Ofﬂoading',
             'Optimal cell clustering andactivation for energy saving in load-coupled wireless networks,',
             'K. Murty, Linear programming, Wiley, 1983.',
@@ -47,3 +47,5 @@ class TestExPDF:
         
         assert self.expdf_parser.refs == aims
 
+
+TestExPDF.setup_class()
