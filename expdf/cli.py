9c9c9c365c236d4ee9e587fbe3b0c2f1d5a67930
LampV <1071952403@qq.com>
2020-05-06 11:16:29 +0800

    add logic of glob all pdfs

--- expdf/cli.py
+++ expdf/cli.py
@@ -3,7 +3,7 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-05-02 23:26
+@edit time: 2020-05-06 11:14
 @FilePath: /expdf/cli.py
 @desc:
 Command Line tool to get metadata, references and links from local ot remote PDFs,
@@ -51,15 +51,35 @@ def command_line():
     parser = create_parser()
     args = parser.parse_args()
     
-    print(f'recursive is {args.recursive}')
+    pdfs = []
+    pdf_path = here / args.pdf_path
     
+    # glob all pdfs
+    print(f'recursive is {args.recursive}')
     if args.recursive:
         print(f'find all pdf in {args.pdf_path}')
+        for file in pdf_path.iterdir():
+            print(f'find a file {file}')
+            if file.suffix == '.pdf':
+                pdfs.append(str(file))
     else:
         print(f'find pdf file at {args.pdf_path}')
+        if pdf_path.suffix == '.pdf':
+            pdfs.append(str(pdf_path))
+        else:
+            raise TypeError(f"{args.pdf_path} is not a pdf file")
     
     for append_pdf in args.append_pdfs:
         print(f'append a pdf file {append_pdf}')
+        append_file = here / append_pdf
+        if append_file.suffix == '.pdf':
+            pdfs.append(str(append_file))
+        else:
+            raise TypeError(f"{append_file} is not a pdf file")
+        
+    # assert pdfs is not []
+    if pdfs == []:
+        print(f'no pdf file')
         
     print(f'generate expdf for pdf in pdfs')
     print(f'generate graph')
