ff19fef6bb15321d8e74a4907f3bb53785a2f13e
LampV <1071952403@qq.com>
2020-05-10 10:46:54 +0800

    change import path

--- expdf/__init__.py
+++ expdf/__init__.py
@@ -3,11 +3,11 @@
 """
 @author: Jiawei Wu
 @create time: 1970-01-01 08:00
-@edit time: 2020-05-06 11:41
-@FilePath: /expdf/__init__.py
+@edit time: 2020-05-10 10:21
+@FilePath: /expdf/expdf/__init__.py
 @desc: init file of expdf
 """
 
-from .graph import LocalPDFNode, Graph
+from .pdfnode import LocalPDFNode, PDFNode
 from .visualize import render
 from .parser import ExPDFParser
